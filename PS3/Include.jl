16eb99d44aeae66ab6702024c471199d87806af2
Sanjana Uddin <ssu3@cornell.edu>
2023-04-16 22:35:48 -0400

    Update Include.jl

--- PS3/Include.jl
+++ PS3/Include.jl
@@ -8,3 +8,6 @@ using IterativeSolvers
 
 # load my codes -
 # TODO: Fill in the include statements with your files
+include(joinpath(_PATH_TO_SRC, "Types.jl"))
+include(joinpath(_PATH_TO_SRC, "Factory.jl"))
+include(joinpath(_PATH_TO_SRC, "Solvers.jl"))
