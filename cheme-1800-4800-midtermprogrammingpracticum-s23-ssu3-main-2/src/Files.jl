7f6b78e0686e5afe73230bd8893223773b2a4165
Sanjana Uddin <ssu3@cornell.edu>
2023-04-20 17:55:35 -0400

    Delete Files.jl

--- cheme-1800-4800-midtermprogrammingpracticum-s23-ssu3-main-2/src/Files.jl
+++
@@ -1,8 +0,0 @@
-"""
-readmodelfile(path::String) -> Dict{String,Any}
-
-The function reads a JSON file in the given path and converts it into a dictionary. 
-"""
-function readmodelfile(path::String)::Dict{String,Any}
-    return JSON.parsefile(path)
-end
